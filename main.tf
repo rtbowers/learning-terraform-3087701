5f887bc2e4be014eccfd3007edfbecdc71fa70ab
rtbowers <95734737+rtbowers@users.noreply.github.com>
2024-06-15 11:53:54 -0400

    Add name for allow everything out

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
