20ecd15170d1285ef270f19e961076f4373c6efb
rtbowers <95734737+rtbowers@users.noreply.github.com>
2024-06-15 13:40:23 -0400

    Remove unnecessary code

--- variables.tf
+++ variables.tf
@@ -26,14 +26,6 @@ variable "environment" {
     name           = "dev"
     network_prefix = "10.0"
   }
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
 }
 
 variable "asg_min_size" {
